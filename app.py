66ba83c23a0da3b48ceae399e7545dccbeb417ed
Davronbekn202 <davronbekn202@gmail.com>
2026-01-01 23:50:20 +0500

    first flask website

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('link.html')
+
+
+@app.route('/main')
+def main():
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
